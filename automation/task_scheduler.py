fdbf1b39a23d749fdfbe8087d7ceb0743c1d1b9f
Victor Sonck <victor.sonck@gmail.com>
2022-06-15 10:55:00 +0200

    added scheduler and trigger

--- automation/task_scheduler.py
+++ automation/task_scheduler.py
@@ -1,13 +1,29 @@
 from clearml import Task
 from clearml.automation import TaskScheduler
+from datetime import datetime
 
 
-scheduler = TaskScheduler(sync_frequency_minutes=1)
+# Create the scheduler and make it poll quickly for demo purpose
+scheduler = TaskScheduler(
+    sync_frequency_minutes=1,
+    force_create_task_project='Project MLops',
+    force_create_task_name='Scientist #1 Scheduler'
+)
+
+# Get the task that we want to rerun
 get_data_task = Task.get_task(project_name='Project Data Scientist #1', task_name='get data')
+today = datetime.today().strftime('%Y-%m-%d')
+
+# Add the scheduler based on task above and override current date to get newest data
 scheduler.add_task(
     schedule_task_id=get_data_task.id,
     queue='CPU Queue',
-    minute=1,
-    execute_immediately=True
+    hour=8,
+    minute=30,
+    weekdays=['friday'],
+    execute_immediately=True,
+    task_parameters={'General/query': f'SELECT * FROM asteroids WHERE strftime("%Y-%m-%d", `date`) <= strftime("%Y-%m-%d", "{today}")'}
 )
+
+# Start the scheduler
 scheduler.start_remotely(queue='services')
---
+++ automation/task_trigger.py
@@ -0,0 +1,29 @@
+from clearml import Task
+from clearml.automation import TriggerScheduler
+
+# Create the scheduler and make it poll quickly for demo purpose
+scheduler = TriggerScheduler(
+    pooling_frequency_minutes=1,
+    sync_frequency_minutes=1,
+    force_create_task_project='Project MLops',
+    force_create_task_name='Scientist #1 Trigger'
+)
+
+# Get the pipeline ID that we want to clone when a new dataset version drops
+training_pipeline = Task.get_task(
+    task_id='2f0456be3f46481699fe4e2d700ec1e9'
+)
+
+# Add the actual trigger and enqueue the pipeline in services because we're
+# cloning the pipelinecontroller. The controller itself will properly enqueue
+# the different nodes itself.
+scheduler.add_dataset_trigger(
+    schedule_task_id=training_pipeline.id,
+    schedule_queue='services',
+    name='Scientist #1 New Data Training',
+    trigger_project='Project Data Scientist #1',
+    trigger_name='raw dataset'
+)
+
+# Start the scheduler
+scheduler.start_remotely(queue='services')
